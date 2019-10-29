647eb11d2003d99a7c344bcefc6603608c49b917
Oscar Dieste <odieste@fi.upm.es>
2019-10-29 14:37:57 +0100

    Initial commit

---
+++ src/main/java/es/upm/grise/profundizacion2019/mavenExample/App.java
@@ -0,0 +1,13 @@
+package es.upm.grise.profundizacion2019.mavenExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/es/upm/grise/profundizacion2019/mavenExample/AppTest.java
@@ -0,0 +1,13 @@
+package es.upm.grise.profundizacion2019.mavenExample;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
